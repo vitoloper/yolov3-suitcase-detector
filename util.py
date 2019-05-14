0617a9e7ad00959cb8e2affdd04c1f4ea6d3777d
vitoloper <vitoloper@users.noreply.github.com>
2019-05-14 16:27:04 +0200

    Add threshold and nms

--- util.py
+++ util.py
@@ -58,3 +58,151 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
     prediction[:,:,:4] *= stride
     
     return prediction
+
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes 
+    
+    
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    
+    #Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+ 
+    #Union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    
+    return iou
+
+def unique(tensor):
+    """
+    Get the classes present in a single image (since there can be multiple true detection of the same class)
+    """
+
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+    
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
+    """
+    Use NMS (non maximum suppression) to obtain the 'true' detections
+
+    """
+
+    # For each of the bbox having an objectness score below a threshold
+    # we set the value of it's attribute to zero.
+    conf_mask = (prediction[:,:,4] > confidence).float().unsqueeze(2)
+    prediction = prediction*conf_mask
+
+    # Convert (center_x, center_y, width, height) to (top-left-corner_x, top-left_corner_y, right-bottom-corner_x, right-bottom-corner_y)
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:,:,0] = (prediction[:,:,0] - prediction[:,:,2]/2)
+    box_corner[:,:,1] = (prediction[:,:,1] - prediction[:,:,3]/2)
+    box_corner[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2) 
+    box_corner[:,:,3] = (prediction[:,:,1] + prediction[:,:,3]/2)
+    prediction[:,:,:4] = box_corner[:,:,:4]
+
+    # NOTE: the number of 'true' detections in every image may be different.
+    # Therefore, confidence thresholding and NMS has to be done for one image at once.
+    batch_size = prediction.size(0)
+
+    write = False
+
+    for ind in range(batch_size):
+        image_pred = prediction[ind]          #image Tensor
+           #confidence thresholding 
+           #NMS
+
+        max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        seq = (image_pred[:,:5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+
+        # Get rid of bbox rows set to zero (which have an object confidence less than threshold)
+        non_zero_ind =  (torch.nonzero(image_pred[:,4]))
+        try:
+            image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
+        except:
+            # No detection, continue
+            continue
+
+        # For PyTorch 0.4 compatibility
+        if image_pred._shape[0] == 0:
+            continue
+
+        # Get the various classes detected in the image
+        img_classes = unique(image_pred_[:,-1]) # -1 index holds the class index
+
+        # Iterate over classes
+        for cls in img_classes:
+            # perform NMS
+
+            #get the detections with one particular class
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+            image_pred_class = image_pred_[class_mask_ind].view(-1,7)
+            
+            #sort the detections such that the entry with the maximum objectness
+            #confidence is at the top
+            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)   #Number of detections
+
+            # Perform NMS
+            for i in range(idx):
+                #Get the IOUs of all boxes that come after the one we are looking at 
+                #in the loop
+                try:
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+                except ValueError:
+                    break
+            
+                except IndexError:
+                    break
+            
+                #Zero out all the detections that have IoU > treshhold
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i+1:] *= iou_mask       
+            
+                #Remove the non-zero entries
+                non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+                
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)      #Repeat the batch_id for as many detections of the class cls in the image
+            seq = batch_ind, image_pred_class
+            
+            # The function write_results outputs a tensor of shape D x 8.
+            # D is the 'true' detection in all of the images, each represented by a row.
+            # Each detection has 8 attributes: index of the image in the batch, 4 corner coords,
+            # objectness score, score of the class with maximum confidence, index of that class.
+
+            # NOTE: write indicates if tensor has been initialized or not.
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+            else:
+                out = torch.cat(seq, 1)
+                output = torch.cat((output, out))
+                
+    # Check if there has been at least one detection, otherwise return 0.
+    try:
+        return output
+    except:
+        return 0
